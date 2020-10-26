1c1429afeaf143bfa96995db91f5b0781ca87071
Azgar <ajo.roso@gmail.com>
2020-10-25 19:55:57 -0600

    cambiando el jenkins por el cd ..

--- pipeline.sh
+++ pipeline.sh
@@ -5,16 +5,18 @@ echo 'newman tests'
 node --version
 cd api/newman
 newman run examen.postman_collection.json -e herokuexamen.postman_environment.json
-cd ../../
+cd ..
+cd ..
 
 echo 'rest assured tests'
 
 cd api/rest_assured
 mvn clean test
-cd ../../
+cd ..
+cd ..
 
 echo 'jmeter tests'
 
 cd load
 jmeter -n -t examen_test_plan.jmx
-cd ../
+cd ..
