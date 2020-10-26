da8eb674bc1b8e7265662e46141d160ab54109b3
Azgar <ajo.roso@gmail.com>
2020-10-25 19:02:21 -0600

    subido 10/25

---
+++ api/restassured/Examen_Rest_Assured/src/test/java/nonAPITest.java
@@ -0,0 +1,25 @@
+import static io.restassured.RestAssured.*;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matcher.*;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+public class nonAPITest extends BaseTest{
+
+    private static String RESOURCE = "/v1/examen/status";
+
+    @Test
+    public void firstExercise() {
+
+    when().
+        get(String.format("%s",RESOURCE))
+    .then()
+        .statusCode(200)
+        .body("status", equalTo("Listos para el examen"));
+
+}
+}
+
+/*{
+        "status": "Listos para el examen"
+        }*/
